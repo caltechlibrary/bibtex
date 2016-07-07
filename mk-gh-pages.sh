54554987e9b2a45e2020c4a03b2a6e2c291a7c8a
R. S. Doiel <rsdoiel@gmail.com>
2016-07-07 15:13:54 -0700

    snapshot

--- mk-gh-pages.sh
+++ mk-gh-pages.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 echo "Generating website with shorthand"
+echo "Rendering index.html"
 shorthand -e "{{pageContent}} :[<: index.md" page.shorthand > index.html
+echo "Rendering installation.html"
 shorthand -e "{{pageContent}} :[<: INSTALL.md" page.shorthand > installation.html
+echo "Rendering readme.html"
 shorthand -e "{{pageContent}} :[<: README.md" page.shorthand > readme.html
+echo "Rendering license.html"
 shorthand -e "{{pageContent}} :[<: LICENSE" page.shorthand > license.html
