191c807b8910ad6e342115f8bab2c1edf3be9414
R. S. Doiel <rsdoiel@gmail.com>
2016-07-08 11:51:46 -0700

    test docs and website

--- mk-website.sh
+++ mk-website.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+function softwareCheck() {
+    for CMD in shorthand; do
+        APP=$(which $CMD)
+        if [ "$APP" = "" ]; then
+            echo "Skipping, missing $CMD"
+            exit 1;
+        fi 
+    done
+}
+
 function mkPage () {
     nav="$1"
     content="$2"
@@ -7,10 +17,12 @@ function mkPage () {
 
     echo "Rendering $html from $content and $nav"
     shorthand \
-        -e "{{navContent}} :[<: $nav" \
-        -e "{{pageContent}} :[<: $content" \
+        -e "{{navContent}} :import-markdown: $nav" \
+        -e "{{pageContent}} :import-markdown: $content" \
         page.shorthand > $html
 }
+
+softwareCheck
 echo "Generating website with shorthand"
 mkPage nav.md index.md index.html
 mkPage nav.md README.md readme.html
