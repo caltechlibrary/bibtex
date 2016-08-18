0eec8354429f637e21b579daca9a48e62b3f0830
R. S. Doiel <rsdoiel@gmail.com>
2016-08-17 21:55:26 -0700

    Migrating website to mkpage from shorthand

--- mk-webapp.sh
+++ mk-webapp.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 CWD=$(pwd)
 cd webapp
-shorthand index.shorthand > index.html
-shorthand bibfilter.shorthand > bibfilter.html
-shorthand bibmerge.shorthand > bibmerge.html
-shorthand bibscrape.shorthand > bibscrape.html
+echo "Building gopherjs parts"
 gopherjs build
+echo "Generating index.html"
+mkpage "nav=nav.md" index.tmpl > index.html
+echo "Generating bibfilter.html"
+mkpage "nav=nav.md" bibfilter.tmpl > bibfilter.html
+echo "Generating bibmerge.html"
+mkpage "nav=nav.md" bibmerge.tmpl > bibmerge.html
+echo "Generating bibscrape.html"
+mkpage "nav=nav.md" bibscrape.tmpl > bibscrape.html
 cd "$CWD"
--- mk-website.sh
+++ mk-website.sh
@@ -16,10 +16,10 @@ function mkPage () {
     html="$3"
 
     echo "Rendering $html from $content and $nav"
-    shorthand \
-        -e "{{navContent}} :import-markdown: $nav" \
-        -e "{{pageContent}} :import-markdown: $content" \
-        page.shorthand > $html
+    mkpage \
+        "nav=$nav" \
+        "content=$content" \
+        page.tmpl > $html
 }
 
 softwareCheck
